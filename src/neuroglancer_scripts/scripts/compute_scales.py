5559431093e2762e12d71f830f7db391b30806e8
Yann Leprince <y.leprince@fz-juelich.de>
2018-02-19 00:07:14 +0100

    move downscaling routines to dyadic_pyramid module

--- src/neuroglancer_scripts/scripts/compute_scales.py
+++ src/neuroglancer_scripts/scripts/compute_scales.py
@@ -8,138 +8,11 @@
 
 import sys
 
-import numpy as np
-from tqdm import tqdm
-
 import neuroglancer_scripts.accessor
 import neuroglancer_scripts.chunk_encoding
+import neuroglancer_scripts.dyadic_pyramid
 import neuroglancer_scripts.downscaling
 from neuroglancer_scripts import precomputed_io
-
-
-def create_next_scale(info, source_scale_index, downscaler,
-                      chunk_reader, chunk_writer):
-    # Key is the resolution in micrometres
-    old_scale_info = info["scales"][source_scale_index]
-    new_scale_info = info["scales"][source_scale_index + 1]
-    old_chunk_size = old_scale_info["chunk_sizes"][0]
-    new_chunk_size = new_scale_info["chunk_sizes"][0]
-    old_key = old_scale_info["key"]
-    new_key = new_scale_info["key"]
-    old_size = old_scale_info["size"]
-    new_size = new_scale_info["size"]
-    dtype = np.dtype(info["data_type"]).newbyteorder("<")
-    num_channels = info["num_channels"]
-    downscaling_factors = [1 if os == ns else 2
-                           for os, ns in zip(old_size, new_size)]
-    if new_size != [(os - 1) // ds + 1
-                    for os, ds in zip(old_size, downscaling_factors)]:
-        raise ValueError("Unsupported downscaling factor between scales "
-                         "{} and {} (only 1 and 2 are supported)"
-                         .format(old_key, new_key))
-
-    downscaler.check_factors(downscaling_factors)
-
-    half_chunk = [osz // f
-                  for osz, f in zip(old_chunk_size, downscaling_factors)]
-    chunk_fetch_factor = [nsz // hc
-                          for nsz, hc in zip(new_chunk_size, half_chunk)]
-
-    def load_and_downscale_old_chunk(z_idx, y_idx, x_idx):
-        xmin = old_chunk_size[0] * x_idx
-        xmax = min(old_chunk_size[0] * (x_idx + 1), old_size[0])
-        ymin = old_chunk_size[1] * y_idx
-        ymax = min(old_chunk_size[1] * (y_idx + 1), old_size[1])
-        zmin = old_chunk_size[2] * z_idx
-        zmax = min(old_chunk_size[2] * (z_idx + 1), old_size[2])
-        old_chunk_coords = (xmin, xmax, ymin, ymax, zmin, zmax)
-
-        chunk = chunk_reader.read_chunk(old_key, old_chunk_coords)
-
-        return downscaler.downscale(chunk, downscaling_factors)
-
-    chunk_range = ((new_size[0] - 1) // new_chunk_size[0] + 1,
-                   (new_size[1] - 1) // new_chunk_size[1] + 1,
-                   (new_size[2] - 1) // new_chunk_size[2] + 1)
-    for x_idx, y_idx, z_idx in tqdm(
-            np.ndindex(chunk_range), total=np.prod(chunk_range),
-            desc="computing scale {}".format(new_key),
-            unit="chunks", leave=True):
-        xmin = new_chunk_size[0] * x_idx
-        xmax = min(new_chunk_size[0] * (x_idx + 1), new_size[0])
-        ymin = new_chunk_size[1] * y_idx
-        ymax = min(new_chunk_size[1] * (y_idx + 1), new_size[1])
-        zmin = new_chunk_size[2] * z_idx
-        zmax = min(new_chunk_size[2] * (z_idx + 1), new_size[2])
-        new_chunk_coords = (xmin, xmax, ymin, ymax, zmin, zmax)
-        new_chunk = np.empty(
-            [num_channels, zmax - zmin, ymax - ymin, xmax - xmin],
-            dtype=dtype
-        )
-        new_chunk[:, :half_chunk[2], :half_chunk[1],
-                  :half_chunk[0]] = (
-                      load_and_downscale_old_chunk(
-                          z_idx * chunk_fetch_factor[2],
-                          y_idx * chunk_fetch_factor[1],
-                          x_idx * chunk_fetch_factor[0]))
-        if new_chunk.shape[1] > half_chunk[2]:
-            new_chunk[:, half_chunk[2]:, :half_chunk[1],
-                      :half_chunk[0]] = (
-                          load_and_downscale_old_chunk(
-                              z_idx * chunk_fetch_factor[2] + 1,
-                              y_idx * chunk_fetch_factor[1],
-                              x_idx * chunk_fetch_factor[0]))
-        if new_chunk.shape[2] > half_chunk[1]:
-            new_chunk[:, :half_chunk[2], half_chunk[1]:,
-                      :half_chunk[0]] = (
-                          load_and_downscale_old_chunk(
-                              z_idx * chunk_fetch_factor[2],
-                              y_idx * chunk_fetch_factor[1] + 1,
-                              x_idx * chunk_fetch_factor[0]))
-        if (new_chunk.shape[1] > half_chunk[2]
-                and new_chunk.shape[2] > half_chunk[1]):
-            new_chunk[:, half_chunk[2]:, half_chunk[1]:,
-                      :half_chunk[0]] = (
-                          load_and_downscale_old_chunk(
-                              z_idx * chunk_fetch_factor[2] + 1,
-                              y_idx * chunk_fetch_factor[1] + 1,
-                              x_idx * chunk_fetch_factor[0]))
-        if new_chunk.shape[3] > half_chunk[0]:
-            new_chunk[:, :half_chunk[2], :half_chunk[1],
-                      half_chunk[0]:] = (
-                          load_and_downscale_old_chunk(
-                              z_idx * chunk_fetch_factor[2],
-                              y_idx * chunk_fetch_factor[1],
-                              x_idx * chunk_fetch_factor[0] + 1))
-        if (new_chunk.shape[1] > half_chunk[2]
-                and new_chunk.shape[3] > half_chunk[0]):
-            new_chunk[:, half_chunk[2]:, :half_chunk[1],
-                      half_chunk[0]:] = (
-                          load_and_downscale_old_chunk(
-                              z_idx * chunk_fetch_factor[2] + 1,
-                              y_idx * chunk_fetch_factor[1],
-                              x_idx * chunk_fetch_factor[0] + 1))
-        if (new_chunk.shape[2] > half_chunk[1]
-                and new_chunk.shape[3] > half_chunk[0]):
-            new_chunk[:, :half_chunk[2], half_chunk[1]:,
-                      half_chunk[0]:] = (
-                          load_and_downscale_old_chunk(
-                              z_idx * chunk_fetch_factor[2],
-                              y_idx * chunk_fetch_factor[1] + 1,
-                              x_idx * chunk_fetch_factor[0] + 1))
-        if (new_chunk.shape[1] > half_chunk[2]
-                and new_chunk.shape[2] > half_chunk[1]
-                and new_chunk.shape[3] > half_chunk[0]):
-            new_chunk[:, half_chunk[2]:, half_chunk[1]:,
-                      half_chunk[0]:] = (
-                          load_and_downscale_old_chunk(
-                              z_idx * chunk_fetch_factor[2] + 1,
-                              y_idx * chunk_fetch_factor[1] + 1,
-                              x_idx * chunk_fetch_factor[0] + 1))
-
-        chunk_writer.write_chunk(
-            new_chunk.astype(dtype), new_key, new_chunk_coords
-        )
 
 
 def compute_scales(work_dir=".", downscaling_method="average", options={}):
@@ -152,9 +25,9 @@ def compute_scales(work_dir=".", downscaling_method="average", options={}):
     )
     downscaler = neuroglancer_scripts.downscaling.get_downscaler(
         downscaling_method, options)
-    for i in range(len(pyramid_io.info["scales"]) - 1):
-        create_next_scale(pyramid_io.info, i, downscaler,
-                          pyramid_io, pyramid_io)
+    neuroglancer_scripts.dyadic_pyramid.compute_dyadic_scales(
+        pyramid_io, downscaler
+    )
 
 
 def parse_command_line(argv):

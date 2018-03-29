373eac919cd451198a7d7a19db7a8db81b33d908
Yann Leprince <y.leprince@fz-juelich.de>
2018-03-29 17:28:46 +0200

    Use pathlib to simplify handling of paths in FileAccessor

--- src/neuroglancer_scripts/file_accessor.py
+++ src/neuroglancer_scripts/file_accessor.py
@@ -10,7 +10,7 @@ API.
 """
 
 import gzip
-import os.path
+import pathlib
 
 import neuroglancer_scripts.accessor
 from neuroglancer_scripts.accessor import _CHUNK_PATTERN_FLAT, DataAccessError
@@ -32,7 +32,7 @@ NO_COMPRESS_MIME_TYPES = {
 class FileAccessor(neuroglancer_scripts.accessor.Accessor):
     """Access a Neuroglancer pre-computed pyramid on the local file system.
 
-    :param str base_dir: the directory containing the pyramid
+    :param str base_dir: path to the directory containing the pyramid
     :param bool flat: use a flat file layout (see :ref:`layouts`)
     :param bool gzip: compress chunks losslessly with gzip
     """
@@ -41,7 +41,7 @@ class FileAccessor(neuroglancer_scripts.accessor.Accessor):
     can_write = True
 
     def __init__(self, base_dir, flat=False, gzip=True):
-        self.base_dir = base_dir
+        self.base_path = pathlib.Path(base_dir)
         if flat:
             self.chunk_pattern = _CHUNK_PATTERN_FLAT
         else:
@@ -49,40 +49,45 @@ class FileAccessor(neuroglancer_scripts.accessor.Accessor):
         self.gzip = gzip
 
     def fetch_file(self, relative_path):
-        file_path = os.path.join(self.base_dir, relative_path)
-        if not file_path.startswith(os.path.join(self.base_dir, "")):
-            raise ValueError("only relative paths pointing under base_dir are "
-                             "accepted")
-        if os.path.isfile(file_path):
-            f = open(file_path, "rb")
-        elif os.path.isfile(file_path + ".gz"):
-            f = gzip.open(file_path + ".gz", "rb")
+        relative_path = pathlib.Path(relative_path)
+        file_path = self.base_path / relative_path
+        if ".." in file_path.relative_to(self.base_path).parts:
+            raise ValueError("only relative paths pointing under base_path "
+                             "are accepted")
+        if file_path.is_file():
+            f = file_path.open("rb")
+        elif file_path.with_name(file_path.name + ".gz").is_file():
+            f = gzip.open(str(file_path.with_name(file_path.name + ".gz")),
+                          "rb")
         else:
             raise DataAccessError("Cannot find {0} in {1}".format(
-                relative_path, self.base_dir))
+                relative_path, self.base_path))
         try:
             with f:
                 return f.read()
         except OSError as exc:
             raise DataAccessError(
                 "Error fetching {1}: {2}" .format(
-                    relative_path, self.base_dir, exc)) from exc
+                    relative_path, self.base_path, exc)) from exc
 
     def store_file(self, relative_path, buf,
                    mime_type="application/octet-stream",
                    overwrite=False):
-        file_path = os.path.join(self.base_dir, relative_path)
-        if not file_path.startswith(self.base_dir + os.path.sep):
-            raise ValueError("only relative paths pointing under base_dir are "
-                             "accepted")
+        relative_path = pathlib.Path(relative_path)
+        file_path = self.base_path / relative_path
+        if ".." in file_path.relative_to(self.base_path).parts:
+            raise ValueError("only relative paths pointing under base_path "
+                             "are accepted")
         mode = "wb" if overwrite else "xb"
         try:
-            os.makedirs(os.path.dirname(file_path), exist_ok=True)
+            file_path.parent.mkdir(parents=True, exist_ok=True)
             if self.gzip and mime_type not in NO_COMPRESS_MIME_TYPES:
-                with gzip.open(file_path + ".gz", mode) as f:
+                with gzip.open(
+                        str(file_path.with_name(file_path.name + ".gz")),
+                        mode) as f:
                     f.write(buf)
             else:
-                with open(file_path, mode) as f:
+                with file_path.open(mode) as f:
                     f.write(buf)
         except OSError as exc:
             raise DataAccessError("Error storing {0}: {1}"
@@ -91,14 +96,16 @@ class FileAccessor(neuroglancer_scripts.accessor.Accessor):
     def fetch_chunk(self, key, chunk_coords):
         f = None
         for pattern in _CHUNK_PATTERN_FLAT, _CHUNK_PATTERN_SUBDIR:
-            chunk_path = self._chunk_filename(key, chunk_coords, pattern)
-            if os.path.isfile(chunk_path):
-                f = open(chunk_path, "rb")
-            elif os.path.isfile(chunk_path + ".gz"):
-                f = gzip.open(chunk_path + ".gz", "rb")
+            chunk_path = self._chunk_path(key, chunk_coords, pattern)
+            if chunk_path.is_file():
+                f = chunk_path.open("rb")
+            elif chunk_path.with_name(chunk_path.name + ".gz").is_file():
+                f = gzip.open(
+                    str(chunk_path.with_name(chunk_path.name + ".gz")), "rb"
+                )
         if f is None:
             raise DataAccessError("Cannot find chunk {0} in {1}".format(
-                self._flat_chunk_basename(key, chunk_coords), self.base_dir))
+                self._flat_chunk_basename(key, chunk_coords), self.base_path))
         try:
             with f:
                 return f.read()
@@ -106,34 +113,36 @@ class FileAccessor(neuroglancer_scripts.accessor.Accessor):
             raise DataAccessError(
                 "Error accessing chunk {0} in {1}: {2}" .format(
                     self._flat_chunk_basename(key, chunk_coords),
-                    self.base_dir, exc)) from exc
+                    self.base_path, exc)) from exc
 
     def store_chunk(self, buf, key, chunk_coords,
                     mime_type="application/octet-stream",
                     overwrite=True):
-        chunk_path = self._chunk_filename(key, chunk_coords)
+        chunk_path = self._chunk_path(key, chunk_coords)
         mode = "wb" if overwrite else "xb"
         try:
-            os.makedirs(os.path.dirname(chunk_path), exist_ok=True)
+            chunk_path.parent.mkdir(parents=True, exist_ok=True)
             if self.gzip and mime_type not in NO_COMPRESS_MIME_TYPES:
-                with gzip.open(chunk_path + ".gz", mode) as f:
+                with gzip.open(
+                        str(chunk_path.with_name(chunk_path.name + ".gz")),
+                        mode) as f:
                     f.write(buf)
             else:
-                with open(chunk_path, mode) as f:
+                with chunk_path.open(mode) as f:
                     f.write(buf)
         except OSError as exc:
             raise DataAccessError(
                 "Error storing chunk {0} in {1}: {2}" .format(
                     self._flat_chunk_basename(key, chunk_coords),
-                    self.base_dir, exc)) from exc
+                    self.base_path, exc)) from exc
 
-    def _chunk_filename(self, key, chunk_coords, pattern=None):
+    def _chunk_path(self, key, chunk_coords, pattern=None):
         if pattern is None:
             pattern = self.chunk_pattern
         xmin, xmax, ymin, ymax, zmin, zmax = chunk_coords
         chunk_filename = pattern.format(
             xmin, xmax, ymin, ymax, zmin, zmax, key=key)
-        return os.path.join(self.base_dir, chunk_filename)
+        return self.base_path / chunk_filename
 
     def _flat_chunk_basename(self, key, chunk_coords):
         xmin, xmax, ymin, ymax, zmin, zmax = chunk_coords

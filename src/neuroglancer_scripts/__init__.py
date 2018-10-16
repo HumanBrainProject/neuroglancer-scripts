40624e155ff3ec9012942744b5c09d91164d5756
Yann Leprince <yann.leprince@cea.fr>
2018-10-16 16:15:07 +0200

    Set the version to 0.2.0

--- src/neuroglancer_scripts/__init__.py
+++ src/neuroglancer_scripts/__init__.py
@@ -8,5 +8,21 @@
 .. todo:: introduction to the high-level APIs
 """
 
-# Version used by setup.py and docs/conf.py
-__version__ = "0.2.0.dev0"
+# Version used by setup.py and docs/conf.py (parsed with a regular expression).
+#
+# Release checklist (based on https://packaging.python.org/):
+# 1.  Ensure that tests pass for all supported Python version (Travis CI),
+#     ensure that the API documentation is complete (sphinx-apidoc -o docs/api/
+#     src/neuroglancer_scripts);
+# 2.  Update the release notes;
+# 3.  Run check-manifest;
+# 4.  Bump the version number in this file;
+# 5.  pip install -U setuptools wheel twine
+# 6.  python setup.py sdist bdist_wheel
+# 7.  twine upload --repository-url https://test.pypi.org/legacy/ dist/*
+# 8.  Commit the updated version number
+# 9.  Tag the commit (git tag -a vX.Y.Z)
+# 10. Bump the version number to something that ends with .dev0 and commit
+# 11. Push the master branch and the new tag to Github
+# 12. twine upload dist/*
+__version__ = "0.2.0"

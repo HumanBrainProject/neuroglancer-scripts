ff8d87f499beb6092620dca33194e1fbc60683eb
Yann Leprince <yann.leprince@cea.fr>
2021-11-15 14:37:34 +0100

    Update the packaging instructions to use PEP517

--- src/neuroglancer_scripts/__init__.py
+++ src/neuroglancer_scripts/__init__.py
@@ -15,18 +15,17 @@
 #     ensure that the API documentation is complete (sphinx-apidoc -o docs/api/
 #     src/neuroglancer_scripts);
 # 2.  Update the release notes;
-# 3.  Run check-manifest;
-# 4.  Bump the version number in this file;
-# 5.  pip install -U setuptools wheel twine
-# 6.  python setup.py sdist bdist_wheel
-# 7.  twine upload --repository-url https://test.pypi.org/legacy/ dist/*
-# 8.  Commit the updated version number
-# 9.  Tag the commit (git tag -a vX.Y.Z). The release notes for the last
+# 3.  Bump the version number in this file;
+# 4.  pip install -U build twine
+# 5.  python3 -m build
+# 6.  twine upload --repository-url https://test.pypi.org/legacy/ dist/*
+# 7.  Commit the updated version number
+# 8.  Tag the commit (git tag -a vX.Y.Z). The release notes for the last
 #     version should be converted to plain text and included in the tag
 #     message:
 #     pandoc -t plain docs/release-notes.rst
-# 10. Bump the version number in this file to something that ends with .dev0
+# 9.  Bump the version number in this file to something that ends with .dev0
 #     and commit
-# 11. Push the master branch and the new tag to Github
-# 12. twine upload dist/*
+# 10. Push the master branch and the new tag to Github
+# 11. twine upload dist/*
 __version__ = "0.4.0.dev0"
